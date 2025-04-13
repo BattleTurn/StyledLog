fcab79a70a6f6149dc9b96fe482c6f86a7f7d64a
Echo Of Steel <thanhanforwork@gmail.com>
2025-04-13 12:26:57 +0700

    Update Colorful.cs

--- Runtime/Colorful.cs
+++ Runtime/Colorful.cs
@@ -1,13 +1,13 @@
-using UnityEngine;
 using System;
 using System.Text;
+using UnityEngine;
 
 namespace UniLog
 {
     /// <summary>
     /// Utility class for displaying colored debug messages in the Unity console.
     /// </summary>
-    public static class Colorful
+    public static class Debug
     {
         public delegate string FormatDelegate(string message, params object[] parameters);
         public delegate string StringBuilderAppends(params object[] parameters);
@@ -20,10 +20,82 @@ namespace UniLog
         /// </summary>
         /// <param name="message">The message to log</param>
         /// <param name="hexColor">The color in hexadecimal format (e.g., "FF0000" for red)</param>
+        /// <param name="parameters">Additional parameters for formatting the message</param>
+        /// <returns>The formatted log message</returns>
+        public static string Log(object message, string hexColor, params object[] parameters)
+        {
+            return Log(message, hexColor, UnityEngine.Debug.Log, parameters);
+        }
+
+        /// <summary>
+        /// Log a message with RGB values (0-255)
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="color">color (0-255)</param>
+        /// <param name="parameters">Additional parameters for formatting the message</param>
+        /// <returns>The formatted log message</returns>
+        public static string Log(object message, Color color, params object[] parameters)
+        {
+            return Log(message, color, UnityEngine.Debug.Log, parameters);
+        }
+
+        /// <summary>
+        /// Log a warning message with a specific color.
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="hexColor">The color in hexadecimal format (e.g., "FF0000" for red)</param>
+        /// <param name="parameters">Additional parameters for formatting the message</param>
+        /// <returns>The formatted log message</returns>
+        public static string LogWarning(object message, string hexColor, params object[] parameters)
+        {
+            return Log(message, hexColor, UnityEngine.Debug.LogWarning, parameters);
+        }
+
+        /// <summary>
+        /// Log a warning message with RGB values (0-255)
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="color">color (0-255)</param>
+        /// <param name="parameters">Additional parameters for formatting the message</param>
+        /// <returns>The formatted log message</returns>
+        public static string LogWarning(object message, Color color, params object[] parameters)
+        {
+            return Log(message, color, UnityEngine.Debug.LogWarning, parameters);
+        }
+
+        /// <summary>
+        /// Log a error message with a specific color.
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="hexColor">The color in hexadecimal format (e.g., "FF0000" for red)</param>
+        /// <param name="parameters">Additional parameters for formatting the message</param>
+        /// <returns>The formatted log message</returns>
+        public static string LogError(object message, string hexColor, params object[] parameters)
+        {
+            return Log(message, hexColor, UnityEngine.Debug.LogError, parameters);
+        }
+
+        /// <summary>
+        /// Log a error message with RGB values (0-255)
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="color">color (0-255)</param>
+        /// <param name="parameters">Additional parameters for formatting the message</param>
+        /// <returns>The formatted log message</returns>
+        public static string LogError(object message, Color color, params object[] parameters)
+        {
+            return Log(message, color, UnityEngine.Debug.LogError, parameters);
+        }
+
+        /// <summary>
+        /// Log a message with a specific color.
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="hexColor">The color in hexadecimal format (e.g., "FF0000" for red)</param>
         /// <param name="doLog">The logging action to perform</param>
         /// <param name="parameters">Additional parameters for formatting the message</param>
         /// <returns>The formatted log message</returns>
-        public static string Log(string message, string hexColor, Action<object> doLog, params object[] parameters)
+        private static string Log(object message, string hexColor, Action<object> doLog, params object[] parameters)
         {
             if (hexColor.Contains("#"))
             {
@@ -37,7 +109,10 @@ namespace UniLog
         /// </summary>
         /// <param name="message">The message to log</param>
         /// <param name="color">color (0-255)</param>
-        public static string Log(string message, Color color, Action<object> doLog, params object[] parameters)
+        /// <param name="doLog">The logging action to perform</param>
+        /// <param name="parameters">Additional parameters for formatting the message</param>
+        /// <returns>The formatted log message</returns>
+        private static string Log(object message, Color color, Action<object> doLog, params object[] parameters)
         {
             string hexColor = ColorUtility.ToHtmlStringRGB(color);
             LogHex(message, hexColor, doLog, parameters);
@@ -49,7 +124,7 @@ namespace UniLog
         /// </summary>
         /// <param name="message">The message to log</param>
         /// <param name="hexColor">The color in hexadecimal format (e.g., "FF0000" for red)</param>
-        private static string LogHex(string message, string hexColor, Action<object> doLog, params object[] parameters)
+        private static string LogHex(object message, string hexColor, Action<object> doLog, params object[] parameters)
         {
             string log = string.Empty;
             if (onLogEvent != null)
@@ -64,7 +139,7 @@ namespace UniLog
             return log;
         }
 
-        private static string HandleStringBuilderEvent(string message, string hexColor, object[] parameters)
+        private static string HandleStringBuilderEvent(object message, string hexColor, object[] parameters)
         {
             string log;
             if (onStringBuilderAppendEvent != null)
